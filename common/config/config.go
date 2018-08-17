0c1cb983e165b7ed70d346bcd3def873ac57aac0
Hobson <mysoftzhs@gmail.com>
2018-08-17 18:58:57 +0800

    modify some files

---
+++ common/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+type Server struct {
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+}
+
+type Database struct {
+	Address  []string `json:"address"`
+	Username string   `json:"username"`
+	Password string   `json:"password"`
+	AuthDB   string   `json:"auth_db"`
+}
+
+type Config struct {
+	Server    Server              `json:"server"`
+	Databases map[string]Database `json:"databases"`
+}
