c0b3f1e681f7f49b4a7d3573ff85fc09358e155d
Hobson <mysoftzhs@gmail.com>
2018-08-26 01:30:40 +0800

    Add config process

--- common/config/config.go
+++ common/config/config.go
@@ -1,18 +1,53 @@
 package config
 
-type Server struct {
-	Address string `json:"address"`
-	Port    int    `json:"port"`
+import (
+	"flag"
+	mconfig "github.com/micro/go-config"
+	"github.com/micro/go-config/encoder/yaml"
+	"github.com/micro/go-config/source"
+	"github.com/micro/go-config/source/consul"
+	"github.com/micro/go-config/source/env"
+	mflag "github.com/micro/go-config/source/flag"
+)
+
+var config Config
+
+var consulAddress string
+var consulPrefix string
+
+func init() {
+	flag.StringVar(&consulAddress, "consul_addr", "127.0.0.1:8500", "the consul address default 127.0.0.1:8500")
+	flag.StringVar(&consulPrefix, "consul_prefix", "/teddy/config", "the consul config prefix default /teddy/config")
 }
 
-type Database struct {
-	Address  []string `json:"address"`
-	Username string   `json:"username"`
-	Password string   `json:"password"`
-	AuthDB   string   `json:"auth_db"`
+func Init() error {
+	err := mconfig.Load(
+		env.NewSource(
+			env.WithStrippedPrefix("TEDDY"),
+		),
+		mflag.NewSource(),
+		consul.NewSource(
+			consul.WithAddress(consulAddress),
+			consul.WithPrefix(consulPrefix),
+			consul.StripPrefix(true),
+			source.WithEncoder(yaml.NewEncoder()),
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = mconfig.Scan(&config)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-type Config struct {
-	Server    Server              `json:"server"`
-	Databases map[string]Database `json:"databases"`
+func GetConfig() *Config {
+	return &config
+}
+
+func Watch() <-chan *Config {
+	return nil
 }
