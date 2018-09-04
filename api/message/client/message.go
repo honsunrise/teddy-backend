1770e4c9dedf62073581350296eafcc3e264d3e7
Hobson <mysoftzhs@gmail.com>
2018-09-04 16:56:12 +0800

    modify message use gin

--- api/message/client/message.go
+++ api/message/client/message.go
@@ -1,29 +1,24 @@
 package client
 
 import (
-	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/client"
 
-	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/server"
 	"github.com/zhsyourai/teddy-backend/message/proto"
 )
 
-type notifyKey struct{}
+var messageKey = "__teddy_message_client_key__"
 
 // FromContext retrieves the client from the Context
-func MessageFromContext(ctx context.Context) (proto.MessageService, bool) {
-	c, ok := ctx.Value(notifyKey{}).(proto.MessageService)
+func MessageFromContext(ctx *gin.Context) (proto.MessageService, bool) {
+	c, ok := ctx.Value(messageKey).(proto.MessageService)
 	return c, ok
 }
 
 // Client returns a wrapper for the UaaClient
-func MessageWrapper(service micro.Service) server.HandlerWrapper {
-	client := proto.NewMessageService("com.teddy.srv.notify", service.Client())
-
-	return func(fn server.HandlerFunc) server.HandlerFunc {
-		return func(ctx context.Context, req server.Request, rsp interface{}) error {
-			ctx = context.WithValue(ctx, notifyKey{}, client)
-			return fn(ctx, req, rsp)
-		}
+func MessageNew() gin.HandlerFunc {
+	c := proto.NewMessageService("com.teddy.srv.notify", client.DefaultClient)
+	return func(ctx *gin.Context) {
+		ctx.Set(messageKey, c)
 	}
 }
--- api/message/main.go
+++ api/message/main.go
@@ -1,32 +1,70 @@
 package main
 
 import (
-	"github.com/micro/go-log"
-
-	"github.com/micro/go-micro"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"github.com/zhsyourai/teddy-backend/api/gin-jwt"
 	"github.com/zhsyourai/teddy-backend/api/message/client"
 	"github.com/zhsyourai/teddy-backend/api/message/handler"
+	"github.com/zhsyourai/teddy-backend/common/config"
+	"log"
 
-	"github.com/zhsyourai/teddy-backend/api/message/proto"
+	"github.com/gin-gonic/gin"
+
+	"github.com/micro/go-web"
 )
 
 func main() {
-	// New Service
-	service := micro.NewService(
-		micro.Name("go.micro.api.message"),
-		micro.Version("latest"),
+	flag.Parse()
+
+	err := config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Load config
+	conf := config.GetConfig()
+
+	// Load Jwt PublicKey
+	block, _ := pem.Decode([]byte(conf.JWTPkcs8))
+	parseResult, _ := x509.ParsePKCS8PrivateKey(block.Bytes)
+	key := parseResult.(*rsa.PrivateKey)
+
+	// Create service
+	service := web.NewService(
+		web.Name("go.micro.api.message"),
+		web.Version("latest"),
 	)
 
 	// Initialise service
-	service.Init(
-		// create wrap for the Message srv client
-		micro.WrapHandler(client.MessageWrapper(service)),
-	)
+	service.Init()
+
+	jwtMiddleware, err := gin_jwt.NewGinJwtMiddleware(gin_jwt.MiddlewareConfig{
+		Realm:            "com.teddy.uaa",
+		SigningAlgorithm: "RS512",
+		KeyFunc: func() interface{} {
+			return &key.PublicKey
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create RESTful handler (using Gin)
+	router := gin.Default()
+	router.Use(client.MessageNew())
+	handler.NewMessageHandler(jwtMiddleware).Handler(router)
 
 	// Register Handler
-	proto.RegisterMessageHandler(service.Server(), new(handler.Message))
+	service.Handle("/", router)
 
-	// Run service
+	// Run server
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
