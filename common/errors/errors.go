f23524f02d168ac72c0df4168c3010fa40eaf78a
zhsyourai <mysoftzhs@gmail.com>
2018-12-18 17:03:43 +0800

    Add error

--- common/errors/errors.go
+++ common/errors/errors.go
@@ -4,3 +4,5 @@ import "errors"
 
 var ErrClientNotFound = errors.New("client not found")
 var ErrCaptchaNotCorrect = errors.New("captcha is not match")
+
+var ErrOrderNotCorrect = errors.New("order not correct must be asc or desc")
