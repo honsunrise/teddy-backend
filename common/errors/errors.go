d83358369fa0ab92be0aabed543b6d543adb7147
zhsyourai <mysoftzhs@gmail.com>
2018-12-21 02:13:15 +0800

    modify proto

--- common/errors/errors.go
+++
@@ -1,8 +0,0 @@
-package errors
-
-import "errors"
-
-var ErrClientNotFound = errors.New("client not found")
-var ErrCaptchaNotCorrect = errors.New("captcha is not match")
-
-var ErrOrderNotCorrect = errors.New("order not correct must be asc or desc")
--- common/types/types.go
+++ common/types/types.go
@@ -1,49 +1,13 @@
 package types
 
-import (
-	"errors"
-	"strings"
-)
-
-var ErrOrderNotSupport = errors.New("order not support")
-
 type Paging struct {
 	Page uint32 `json:"page"`
 	Size uint32 `json:"size"`
 }
 
-type Order uint8
-
-const (
-	ASC Order = 1 << iota
-	DESC
-)
-
-func (o Order) String() string {
-	switch o {
-	case ASC:
-		return "ASC"
-	case DESC:
-		return "DESC"
-	default:
-		return "ASC"
-	}
-}
-
-func ParseOrder(order string) (Order, error) {
-	switch strings.ToUpper(order) {
-	case "ASC":
-		return ASC, nil
-	case "DESC":
-		return DESC, nil
-	default:
-		return 0, ErrOrderNotSupport
-	}
-}
-
 type Sort struct {
-	Name  string
-	Order Order
+	Name string
+	ASC  bool
 }
 
 // For config
