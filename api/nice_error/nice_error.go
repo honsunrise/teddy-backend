6bb7691ec18bfcde952a92015a9ec494af928a7c
zhsyourai <mysoftzhs@gmail.com>
2018-12-22 03:58:13 +0800

    add nice error support

--- api/nice_error/nice_error.go
+++ api/nice_error/nice_error.go
@@ -37,26 +37,34 @@ func (ne *niceError) process(c *gin.Context) {
 	c.Next()
 
 	err := c.Errors.Last()
-	if err.IsType(gin.ErrorTypePublic) {
+	if err != nil {
 		realErr := err.Err
-		switch realErr.(type) {
-		case *NiceError:
-			toNe := realErr.(*NiceError)
-			c.JSON(toNe.Code, gin.H{
-				"title":  toNe.Title,
-				"detail": toNe.Detail,
+		if err.IsType(gin.ErrorTypePublic) {
+			switch realErr.(type) {
+			case *NiceError:
+				toNe := realErr.(*NiceError)
+				c.JSON(toNe.Code, gin.H{
+					"title":  toNe.Title,
+					"detail": toNe.Detail,
+				})
+			default:
+				toErr := realErr.(error)
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"title":  "Internal Server Error",
+					"detail": toErr.Error(),
+				})
+			}
+		} else if err.IsType(gin.ErrorTypeBind) {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"title":  "Binding Request Error",
+				"detail": "request content missing field",
 			})
-		default:
+		} else {
 			toErr := realErr.(error)
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"title":  "Internal Server Error",
 				"detail": toErr.Error(),
 			})
 		}
-	} else if err.IsType(gin.ErrorTypeBind) {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"title":  "Binding Request Error",
-			"detail": "request content missing field",
-		})
 	}
 }
